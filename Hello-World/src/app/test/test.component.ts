024d5fd3bd8232d12649875fa3e9b7c76ffa89c5
Aktarul Islam <aktarul857@gmail.com>
2018-03-17 14:09:11 +0600

    class, Style, Event Binding completed

--- Hello-World/src/app/test/test.component.ts
+++ Hello-World/src/app/test/test.component.ts
@@ -17,20 +17,65 @@ import { Component, OnInit } from '@angular/core';
 
   <h2> {{ greetUser() }} </h2>
 
-  <h2> {{ siteURL }} </h2>
+  <h2> {{ siteURL }} </h2><hr>
 
   <!-- Property Binding -->
   <input [id]="myId" type="test" value="Aktarul"><br><br>
   <input id="{{ myId }}" type="test" value="Aktarul"><br><br>
-  <input bind-disabled ="isDisabled" [id]="myId" type="test" value="Aktarul">
+  <input bind-disabled ="isDisabled" [id]="myId" type="test" value="Aktarul"><br><br><hr>
+
+  <!-- Class Binding -->
+  <h2 class="text-success text-special"> {{ name.toUpperCase() }} </h2>
+  <h2 bind-class = "successClass"> {{ name.toUpperCase() }} </h2>
+  <h2 bind-class.text-danger = "hasError"> {{ name.toUpperCase() }} </h2>
+  <h2 bind-ngClass = "messageClasses"> {{ message }} </h2><hr><br>
+
+  <!-- Style Binding -->
+  <h2 [style.color]= " hasError ? 'red' : 'orange'"> Style Binding 1 </h2>
+  <h2 [style.color]="highlightColor"> Style Binding 2 </h2>
+  <h2 [ngStyle]="titleStyles"> Style Binding 3 </h2><br><hr>
+
+  <!-- Event Binding -->
+  <button (click)="onClick($event)"> Greet </button>
+    {{typeEvent}}<br>{{ greeting }}<br>
+  <button (click)="greeting='Hello World'"> Another Greet </button><hr><br>
   `,
-  styleUrls: ['./test.component.css']
+  styles: [`
+  .text-success {
+    color: green;
+  }
+  .text-danger {
+    color: red;
+  }
+  .text-special {
+    font-style: italic;
+  }
+  `]
 })
 export class TestComponent implements OnInit {
   public name = 'Aktarul';
   public siteURL = window.location.href;
   public myId = 'testId';
   public isDisabled = true;
+
+  public message = 'Hello World';
+  public successClass = 'text-success';
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial
+  };
+
+  public highlightColor = 'Blue';
+  public titleStyles = {
+    color: 'orange',
+    fontStyle: 'italic'
+  };
+
+  public greeting = '';
+  public typeEvent = '';
 
   constructor() { }
 
@@ -39,6 +84,12 @@ export class TestComponent implements OnInit {
 
   greetUser() {
     return 'Hello ' + this.name;
+  }
+
+  onClick(event) {
+    console.log(event);
+    this.greeting = 'Welcome to my first Angular App!';
+    this.typeEvent = 'this is a ' + event.type + ' event.';
   }
 
 }
