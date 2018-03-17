7a7faabd80713f7e5c4986296eca7378e02f3087
Aktarul Islam <aktarul857@gmail.com>
2018-03-17 17:20:45 +0600

    Structure Directives Completed

--- Hello-World/src/app/test/test.component.ts
+++ Hello-World/src/app/test/test.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   <h2>
     Welcome {{ name }}
   </h2>
-
+  <hr><br>
   <!-- Interpolation -->
+  <h3 class="header"> Interpolation </h3>
   <h2> {{ 2 + 2 }}</h2>
   <h2> {{ "Welcome " + name }}</h2>
 
@@ -20,26 +21,85 @@ import { Component, OnInit } from '@angular/core';
   <h2> {{ siteURL }} </h2><hr>
 
   <!-- Property Binding -->
+  <h3 class="header"> Property Binding </h3>
   <input [id]="myId" type="test" value="Aktarul"><br><br>
   <input id="{{ myId }}" type="test" value="Aktarul"><br><br>
   <input bind-disabled ="isDisabled" [id]="myId" type="test" value="Aktarul"><br><br><hr>
 
   <!-- Class Binding -->
+  <h3 class="header"> Class Binding </h3>
   <h2 class="text-success text-special"> {{ name.toUpperCase() }} </h2>
   <h2 bind-class = "successClass"> {{ name.toUpperCase() }} </h2>
   <h2 bind-class.text-danger = "hasError"> {{ name.toUpperCase() }} </h2>
   <h2 bind-ngClass = "messageClasses"> {{ message }} </h2><hr><br>
 
   <!-- Style Binding -->
+  <h3 class="header"> Style Binding </h3>
   <h2 [style.color]= " hasError ? 'red' : 'orange'"> Style Binding 1 </h2>
   <h2 [style.color]="highlightColor"> Style Binding 2 </h2>
   <h2 [ngStyle]="titleStyles"> Style Binding 3 </h2><br><hr>
 
   <!-- Event Binding -->
+  <h3 class="header"> Event Binding </h3>
   <button (click)="onClick($event)"> Greet </button>
     {{typeEvent}}<br>{{ greeting }}<br>
   <button (click)="greeting='Hello World'"> Another Greet </button><hr><br>
+
+  <!-- Template Reference Variables -->
+  <h3 class="header"> Template Reference Variables </h3>
+  <input #myInput type="text">
+  <button (click)="logMessage(myInput.value)" >log</button><hr><br>
+
+  <!-- Two Way Binding-->
+  <h3 class="header"> Two Way Binding </h3>
+  <input [(ngModel)]="newName" type="text">
+  {{ newName }}<hr><br>
+
+  <!-- Structural Directives -->
+  <h3 class="header"> Structural Directives </h3>
+    <!-- ngIf -->
+  <h2 *ngIf="displayValue">
+    Hello World
+  </h2>
+  <button (click)="clickShow()" *ngIf="showShowButton">Show</button>
+  <button (click)="clickHide()" *ngIf="showHideButton">Hide</button>
+  <h2 *ngIf="showText">
+    Now Showing the text
+  </h2>
+  <h2 *ngIf="displayValue2; else elseBlock">
+    Hello World
+  </h2>
+  <ng-template #elseBlock>
+    <h2>
+      the hidden text
+    </h2>
+  </ng-template>
+
+  <div *ngIf="displayValue3; then thenPart; else elsePart"></div>
+  <ng-template #thenPart>
+    <h2>
+      Then Part
+    </h2>
+  </ng-template>
+
+  <ng-template #elsePart>
+    <h2>
+      Else Part
+    </h2>
+  </ng-template>
+      <!-- ngSwitch -->
+    <div [ngSwitch]='color'>
+      <div *ngSwitchCase="'red'">You picked Red color</div>
+      <div *ngSwitchCase="'blue'">You picked Blue color</div>
+      <div *ngSwitchCase="'green'">You picked Green color</div>
+      <div *ngSwitchDefault>Pick again</div>
+    </div>
+      <!-- ngFor -->
+      <div *ngFor = "let Color of colors; index as i; first as f; last as l; odd as o">
+        <h2>{{ i }} {{ Color }} First: {{ f }} Last: {{ l }} Odd: {{ o }} </h2>
+      </div>
   `,
+
   styles: [`
   .text-success {
     color: green;
@@ -49,6 +109,12 @@ import { Component, OnInit } from '@angular/core';
   }
   .text-special {
     font-style: italic;
+  }
+  .header {
+    color: #333300;
+    background: #66ff99;
+    margin-left: 200px;
+    margin-right: 200px;
   }
   `]
 })
@@ -77,6 +143,17 @@ export class TestComponent implements OnInit {
   public greeting = '';
   public typeEvent = '';
 
+  public newName = '';
+
+  displayValue = true;
+  showText = true;
+  showHideButton = true;
+  showShowButton = false;
+  displayValue2 = false;
+  displayValue3 = true;
+  color = 'orange';
+  public colors = ['red', 'blue', 'green', 'yellow'];
+
   constructor() { }
 
   ngOnInit() {
@@ -90,6 +167,21 @@ export class TestComponent implements OnInit {
     console.log(event);
     this.greeting = 'Welcome to my first Angular App!';
     this.typeEvent = 'this is a ' + event.type + ' event.';
+  }
+
+  logMessage(value) {
+    console.log(value);
+  }
+
+  clickShow() {
+    this.showText = true;
+    this.showHideButton = true;
+    this.showShowButton = false;
+  }
+  clickHide() {
+    this.showText = false;
+    this.showHideButton = false;
+    this.showShowButton = true;
   }
 
 }
