8c9b7d805cb0827fa2c49d73fce8c2c0f1ede2df
Aktarul Islam <aktarul857@gmail.com>
2018-03-17 11:44:09 +0600

    interpolation and property binding

--- Hello-World/src/app/test/test.component.ts
+++ Hello-World/src/app/test/test.component.ts
@@ -2,14 +2,43 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+  <h2>
+    Welcome {{ name }}
+  </h2>
+
+  <!-- Interpolation -->
+  <h2> {{ 2 + 2 }}</h2>
+  <h2> {{ "Welcome " + name }}</h2>
+
+  <h2> {{ name.length }} </h2>
+
+  <h2> {{ name.toUpperCase() }} </h2>
+
+  <h2> {{ greetUser() }} </h2>
+
+  <h2> {{ siteURL }} </h2>
+
+  <!-- Property Binding -->
+  <input [id]="myId" type="test" value="Aktarul"><br><br>
+  <input id="{{ myId }}" type="test" value="Aktarul"><br><br>
+  <input bind-disabled ="isDisabled" [id]="myId" type="test" value="Aktarul">
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  public name = 'Aktarul';
+  public siteURL = window.location.href;
+  public myId = 'testId';
+  public isDisabled = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser() {
+    return 'Hello ' + this.name;
   }
 
 }
